3e9560a2f8123ee3a9b04b05d841490d873f2b8c
Flashyyyyyy <64888736+Flashyyyyyy@users.noreply.github.com>
2020-05-22 23:49:00 +0300

    Add files via upload

---
+++ Cheat/vars/vars.h
@@ -0,0 +1,56 @@
+#include <chrono>
+
+
+	
+
+		
+		
+		
+
+
+
+
+		
+	
+
+
+
+
+		 int UpdateCount = 13;
+	
+
+	
+      
+		 bool Activate = false;
+		 bool VisibleCheck = false;
+		 bool IgnoreTeam = false;
+		 int Range = 11;
+		 int Fov = 11;
+	
+	
+		 bool AlwaysDay = true;
+		 bool IgnoreSleepers = false;
+	
+		
+	
+		 int pred = 1;
+		 bool Sped = false;
+		 bool SpiderMan = true;
+		 bool SetRA1 = true;
+		 bool FastReload1 = false;
+		 bool AntiSpread1 = false;
+		 bool Automatic = false;
+		 bool SuperEoka = false;
+		 bool HEALT = false;
+		 bool NoSway = false;
+		 bool SuperBow = false;
+		 bool FakeAdmin = true;
+		 bool FatBullet = false;
+		 bool HighJump = false;
+		 bool AntiRecoil =false ;
+		 bool NEBLO = false;
+		//float JumpValue = 1.25f; 0.05 for funrust ahhaah
+		 int JumpValue = false;
+		
+	
+
